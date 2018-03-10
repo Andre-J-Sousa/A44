92327737fc7ab4eae02c164b0e6e7b8c591105af
Andre Sousa <up201504227@fe.up.pt>
2018-03-10 13:14:23 +0000

    Added base file w/ reading
    Adicionado funcão main e estrutura divisão

---
+++ Atuadores/atuador.c
@@ -0,0 +1,39 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Divisao{
+	char nomeDiv[50];
+	char *Sensores[9];
+	char *Atuadores[9];	
+};
+
+
+void EstruturaCasa(FILE *f){
+	
+}
+
+
+int main(){
+	
+	char *filename1="SensorConfigurations.txt", *filename2="SensorRules.txt";
+
+	FILE *f1, *f2;
+	f1 = fopen(filename1, "r");
+	f2 = fopen(filename2, "r");
+	if(f1==NULL || f2==NULL){
+		if(f1==NULL)
+			printf("ERRO!! Ficheiro %s não existe.\n", filename1);
+		else if(f2==NULL)
+			printf("ERRO!! Ficheiro %s não existe.\n", filename2);
+		return 0;
+	}else
+		printf("Nice\n");
+	
+	EstruturaCasa(f1);
+	
+	
+	return 1;
+}
