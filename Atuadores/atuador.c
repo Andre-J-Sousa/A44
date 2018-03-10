ac2cf1cec933b18b3df221a5b2598f6c07ac77ae
Andre Sousa <up201504227@fe.up.pt>
2018-03-10 15:27:44 +0000

    Criada função EstruturaCasa
    Criada função que lê o ficheiro e separa em nome, sensores e atuadores
    
    NÃO ESTÁ COMPLETA

--- Atuadores/atuador.c
+++ Atuadores/atuador.c
@@ -10,9 +10,44 @@ struct Divisao{
 	char Atuadores[90];	
 };
 
+struct Divisao casa[5];	//Variável Global array das divisões e respetivos sensores
+
 
 void EstruturaCasa(FILE *f){
 	
+	char *str;
+	char lido[100];
+	int i=0, c;
+	
+	while(fgets(lido,100,f) != NULL){
+		c=0;
+		if (lido[strlen(lido)-1]=='\n')	//Confirma se último caracter é \n
+			lido[strlen(lido)-1]='\0';	//Remove \n do fim da string lida
+		
+		str = strtok(lido, ":");
+		while (str != NULL){
+			//Faz coisas
+			{
+				switch (c){
+					case 0:
+						strcpy(casa[i].nomeDiv,str);
+					break;
+					case 1:
+						strcpy(casa[i].Sensores,str);
+					break;
+					case 2:
+						strcpy(casa[i].Atuadores,str);
+					break;
+					default:
+					break;
+				}
+					
+			}		
+			str = strtok(NULL, ":");
+			c++;
+		}	
+		i++;
+	}		
 }
 
 
